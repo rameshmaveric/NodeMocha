53fb5a8070e0042b8aa87bdd24a849fed48b531f
rameshmaveric <rameshuser123@gmail.com>
2017-09-18 15:21:06 +0530

    Update test.js
    dssd

--- test/bdd/test.js
+++ test/bdd/test.js
@@ -7,7 +7,6 @@ describe('Array and String', function(){
       [1,2,3].indexOf(0).should.equal(-1);
     });
   });
-
 	describe('#position Of', function(){
     it("should return position of char in a string 'cool'", function(){
       'cool'.indexOf('c').should.equal(0);
